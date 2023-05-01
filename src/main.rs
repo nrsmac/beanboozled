1114d83dc40a37682089d46eea99603ab095860d
Noah Schill <Noah.Schill@microchip.com>
2023-05-01 13:59:06 -0700

    started I guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
+fn dry_to_cooked(dry_bean_cups: i32):
+   dry_bean_cups*2 
+
 fn main() {
-    println!("Hello, world!");
-}
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    let my_int = my_string.parse::<i32>().unwrap();
+
+} 
